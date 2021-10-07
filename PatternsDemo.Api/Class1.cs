2baa62edacf18870ca30da724ece115974070c59
Vesou <wesol89@gmail.com>
2021-10-07 15:48:29 +0100

    add folders

--- PatternsDemo.Api/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace PatternsDemo.Api
-{
-    public class Class1
-    {
-    }
-}
---
+++ PatternsDemo.Api/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PatternsDemo.Api
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
