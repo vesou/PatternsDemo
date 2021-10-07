d2e49f22bddbd1d953ee47d2156367ccebce3641
Vesou <wesol89@gmail.com>
2021-10-07 15:18:45 +0100

    Initial commit

---
+++ Tests/PatternsDemo.Tests.Api/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace PatternsDemo.Tests.Api
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
